564d9f6e7ef4c37f337fcd315cb642269c3d3396
Rishabh-Varshney <rishabh.varshney1124@gmail.com>
2021-01-23 21:05:19 +0530

    created Log In and User Profile Controller

--- middlewares.js
+++ middlewares.js
@@ -3,5 +3,9 @@ import routes from './routes';
 export const localsMiddleware = (req, res, next) => {
 	res.locals.siteName = 'Streamster';
 	res.locals.routes = routes;
+	res.locals.user = {
+		isAuthenticated: true,
+		id: 1,
+	};
 	next();
 };
